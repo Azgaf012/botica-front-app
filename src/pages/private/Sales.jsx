e71bcabc0b6d73fbd005f8e68bf6e32be7fb8e93
renzosanesp10 <rsancheze1@upao.edu.pe>
2022-06-24 20:38:38 -0500

    Second Commit

--- src/pages/private/Sales.jsx
+++ src/pages/private/Sales.jsx
@@ -12,7 +12,7 @@ export const Sales = () => {
                 <Card
                     header={<h2 className="text-center">Nueva Venta</h2>}
                     footer={
-                        <div className="d-flex justify-content-center align-items-center gap-2">}
+                        <div className="d-flex justify-content-center align-items-center gap-2">
                         <p className="m-0">¿Ya tienes una cuenta?</p>
                         </div> 
                     }
@@ -38,6 +38,43 @@ export const Sales = () => {
                                 />
                         </label>
                     </div>
+                    <h1> Productos</h1>
+                        <input type="search" placeholder="Buscar producto"></input>
+                        <button href="#" className="btn btn-primary">Buscar</button>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Cantidad</th>
+                                <th scope="col">Descripcion</th>
+                                <th scope="col">Precio</th>
+                                <th scope="col">Total</th>
+                                <th scope="col">Acciones</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Ibuprofeno</td>
+                            <td>S/. 2.00</td>
+                            <td>S/. 4.00</td>
+                            <td><a href="" class="btn btn-info">Editar</a></td>
+                            <td><a href="" class="btn btn-danger">Eliminar</a></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">4</th>
+                            <td>Paracetamol</td>
+                            <td>S/. 0.50</td>
+                            <td>S/. 2.00</td>
+                            <td><a href="" class="btn btn-info">Editar</a></td>
+                            <td><a href="" class="btn btn-danger">Eliminar</a></td>
+                        </tr>
+                        
+                    </tbody>
+                    </table>
+                <div>
+                    <h3>Total a Pagar</h3>
+                </div>
                 </form>
                 </Card>
         </div>
