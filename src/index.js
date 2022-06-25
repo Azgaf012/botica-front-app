d7dbc74077eeb12ea32153fbd2852bd065decaaf
renzosanesp10 <rsancheze1@upao.edu.pe>
2022-06-24 20:08:42 -0500

    First Commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Login } from './pages/public/Login'
+import { Register } from './pages/public/Register'
+import { ForgotPassword } from './pages/public/ForgotPassword'
+import { Products } from './pages/private/Products'
+import { Sales } from './pages/private/Sales'
+
+import './styles/Products.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot' element={<ForgotPassword />} />
+      <Route path='/products' element={<Products />} />
+      <Route path='/sales' element={<Sales />} />
+    </Routes>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
